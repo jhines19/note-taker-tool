573e5fc9f49433e87e5ff6c671efcea0f65345cf
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-11-17 10:29:43 -0500

    adding fs functionality

---
+++ Develop/routes/index.js
@@ -0,0 +1,2 @@
+//put other routes in this routes folder,
+//and put everything specific for those routes in their respective files
--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const path = require('path');
-
+//todo: add in api and change it to routes/index.js
+const db = require('./Develop/db/db.json')
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+//GET Route for home page
+app.get('/', (req,res) =>
+    res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+);
+
+//GET Route for notes page
+app.get('/notes', (req,res) => 
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+
